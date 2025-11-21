8e3f43d2581c27d9fd3a48c0bcdc546bf4307283
izzyswe <ikj134@yahoo.com>
2025-11-21 11:02:15 -0500

    created Header.jsx

---
+++ travel-journal/src/components/Header.jsx
@@ -0,0 +1,22 @@
+//No Props are needed as Headers are typically static
+//While lowercase for file name like header.jsx or function is okay
+// it is generally best practice to capitalize the first letter for readability sake
+
+// To import photos and CSS, you must import them as modules 
+// as projects get bigger, its important each component has its own css stylesheet as final production shoves it into one
+// (in this case this small and the root or parent component has css already, so no need to repeat the import for every component for now)
+// In modern JavaScript, external files are treated as modules when imported
+import Globe from "../assets/global.svg"
+
+
+//to make comments inside the html use {} followed by using \*comment*\, 
+//anything inside the curly bracket can have javascript code
+// but generally not reccomended unless its inside the html tags for props or mapping and other specific use cases
+export default function Header(){
+  return (
+    <header> {/*This is a comment! but also inside the return, the parent must only have ONE parent, if none fits, use <></> */}
+      <img src= {Globe} alt="logo"/>
+      <h1>my travel journal</h1>
+    </header>
+  )
+}
--- travel-journal/src/main.jsx
+++ travel-journal/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+//this is the main entry point of creating a react project
+// generally should leave this alone unless you are routing or know why you're modifying this
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
