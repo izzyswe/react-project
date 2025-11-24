9a2a85e45fb8e423508b58d0fffd744908155041
Shin417 <shintaroasoemail@gmail.com>
2025-11-23 19:01:11 -0500

    Changed App.jsx to desplay components

--- travel-journal/src/App.jsx
+++ travel-journal/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 //Importing our components (custom html tags)
 import Header from "./components/Header"
 import Entry from "./components/Entry"
+import UserInput from "./components/UserInput"
 
 //importing our images
 import Cancun from "./assets/places/cancun.png"
@@ -10,45 +11,84 @@ import La from "./assets/places/la.png"
 import Paris from "./assets/places/paris.png"
 import Toronto from "./assets/places/toronto.png"
 
+//importing reac userState
+import { useState } from 'react'
+
 function App() {
+
+  const [entries, setEntries] = useState([
+    {
+      id: 1,
+      img: Cancun,
+      titleImg: 'cancun picture',
+      country: 'Mexico',
+      mapUrl: 'https://shorturl.at/2tr7F',
+      title: 'Mexico - Cancun',
+      date: '01-01-25',
+      description: 'Resort'
+    },
+    {
+      id: 2,
+      img: La,
+      titleImg: 'Los Angeles picture',
+      country: 'USA',
+      mapUrl: 'https://shorturl.at/E6Y2P',
+      title: 'Los Angeles - California',
+      date: '01-01-25',
+      description: 'City of Angels'
+    },
+       {
+      id: 3,
+      img: 'https://images.unsplash.com/photo-1511739001486-6bfe10ce785f?w=400',
+      titleImg: 'Paris',
+      country: 'France',
+      mapUrl: 'https://shorturl.at/rxh3Q',
+      title: 'France - Paris',
+      date: '01-01-25',
+      description: 'Fashion City'
+    },
+    {
+      id: 4,
+      img: 'https://images.unsplash.com/photo-1517935706615-2717063c2225?w=400',
+      titleImg: 'Canada - Toronto',
+      country: 'Canada',
+      mapUrl: 'https://shorturl.at/IBURc',
+      title: 'Ontario - Toronto',
+      date: '01-01-25',
+      description: 'Toronto'
+    }
+  ]);
+
+  const addEntry = (newEntry) => {
+    const entry = {
+      ...newEntry,
+      id: Date.now()
+    };
+    setEntries(prev => [entry, ...prev]);
+  };
+
 
   return (
     <>
       <Header />
-      <Entry
-        img={Cancun} 
-        titleImg="cancun picture"
-        country="Mexico"
-        mapUrl="https://shorturl.at/2tr7F"
-        title="Mexico - Cancun"
-        date="01-01-25"
-        description="Resort" />
-      <Entry
-        img={La}
-        titleImg="Los Angles picture"
-        country="USA"
-        mapUrl="https://shorturl.at/E6Y2P"
-        title="Los Angles - California"
-        date="01-01-25"
-        description="City of Angles" />
-      <Entry
-        img={Paris}
-        titleImg="Paris"
-        country="France"
-        mapUrl="https://shorturl.at/rxh3Q"
-        title="France - Paris"
-        date="01-01-25"
-        description="Fashion City" />
-      <Entry
-        img={Toronto} 
-        titleImg="Canada - Toronto"
-        country="Canada"
-        mapUrl="https://shorturl.at/IBURc"
-        title="Ontario - Toronto"
-        date="01-01-25"
-        description="Toronto" />
+      <UserInput onAddEntry={addEntry} />
+      
+      {entries.map(entry => (
+        <Entry
+          key={entry.id}
+          img={entry.img}
+          titleImg={entry.titleImg}
+          country={entry.country}
+          mapUrl={entry.mapUrl}
+          title={entry.title}
+          date={entry.date}
+          description={entry.description}
+        />
+      ))}
     </>
   )
+
+ 
 }
 
 export default App
